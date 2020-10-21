b6c82c658bfbd831bc53bd9261338453d24cf090
Pedro Neto <pedroceciliocn@gmail.com>
2020-10-21 15:09:05 -0300

    first version of glicemy table

---
+++ proj glicemia tabela.R
@@ -0,0 +1,48 @@
+#carregando
+library(tidyverse)
+glicemias <- read_csv("E:/Users/pedro/Downloads/export(0).csv")
+
+#glicemiast <- read_csv("E:/Users/pedro/Downloads/export(0).csv", col_names = TRUE, cols_only(Data = col_date(format = "%d/%b/%Y"), Hora = "t", `Medição da glicemia (mg/dL)` = "d"))
+
+#glicemiast2 <- read_csv("E:/Users/pedro/Downloads/export(0).csv", col_names = TRUE, col_types = "?t_d_____________________")
+
+View(glicemias)
+
+#install.packages("data.table")
+#library(data.table)
+#glicemiasf <- fread("E:/Users/pedro/Downloads/export(0).csv")
+
+library(lubridate)
+
+glicemias %>% 
+  select(Data, Hora, Identificadores, `Medição da glicemia (mg/dL)`) %>%
+  rename(Glicemia = `Medição da glicemia (mg/dL)`) %>% 
+  mutate(Data = as_date(glicemias$Data, format = "%d/%b/%Y")) -> glicemias_filtd
+
+#no lugadr do mutate 
+##glicemias$Data <- as.date(glicemias$Data)
+
+View(glicemias_filtd)
+
+glicemias_filtd %>% 
+  write_excel_csv2("E:/Users/pedro/Downloads/glicemias_filtradas.csv", col_names = TRUE, na = " ")
+
+
+#glicemias_filtd %>% 
+#  write_excel_csv("E:/Users/pedro/Downloads/glicemias_filtradas_2.csv")
+
+
+#salvando
+install.packages("writexl")
+library(writexl)
+
+
+glicemias_filtd %>% 
+  write_xlsx("E:/Users/pedro/Downloads/glicemias_filtradas_x.xlsx")
+
+
+
+#alternativa usando xlsx
+library(readxl)
+glicemias_x <- read_excel("E:/Users/pedro/Downloads/export(1).xls", col_types = c("date", "t", "skip", "d", "skip"))
+View(glicemias_x)
