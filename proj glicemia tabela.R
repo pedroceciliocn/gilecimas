8f4de0304d1acd97f13b0bda09a920658ca525aa
Pedro Neto <pedroceciliocn@gmail.com>
2020-10-25 14:08:23 -0300

    script edited to include here::here() function where code import and export csv/xlsx files instead of setwd or simple path file that can´t be used in another computer or wordking directory

--- proj glicemia tabela.R
+++ proj glicemia tabela.R
@@ -1,8 +1,9 @@
 #carregando
 library(tidyverse)
-glicemias <- read_csv("E:/Users/pedro/Downloads/export(0).csv")
+#glicemias <- read_csv("E:/Users/pedro/Documents/ADM/ESTATISTICA/R/GLICEMIAS/export(0).csv") essa maneira seria errada por conter o path pessoal
 
-#glicemiast <- read_csv("E:/Users/pedro/Downloads/export(0).csv", col_names = TRUE, cols_only(Data = col_date(format = "%d/%b/%Y"), Hora = "t", `Medição da glicemia (mg/dL)` = "d"))
+glicemias <- read_csv(here::here("export(0).csv")) #usando here::here para carregar o csv da pasta do projeto, independente do pc usado
+#glicemiast <- read_csv("E:/Users/pedro/Downloads/export(0).csv", col_names = TRUE, cols_only(Data = col_date(format = "%d/%b/%Y"), Hora = "t", `Medi??o da glicemia (mg/dL)` = "d"))
 
 #glicemiast2 <- read_csv("E:/Users/pedro/Downloads/export(0).csv", col_names = TRUE, col_types = "?t_d_____________________")
 
@@ -15,8 +16,8 @@ View(glicemias)
 library(lubridate)
 
 glicemias %>% 
-  select(Data, Hora, Identificadores, `Medição da glicemia (mg/dL)`) %>%
-  rename(Glicemia = `Medição da glicemia (mg/dL)`) %>% 
+  select(Data, Hora, Identificadores, `MediÃ§Ã£o da glicemia (mg/dL)`) %>%
+  rename(Glicemia = `MediÃ§Ã£o da glicemia (mg/dL)`) %>% 
   mutate(Data = as_date(glicemias$Data, format = "%d/%b/%Y")) -> glicemias_filtd
 
 #no lugadr do mutate 
@@ -24,8 +25,11 @@ glicemias %>%
 
 View(glicemias_filtd)
 
+#glicemias_filtd %>% 
+#  write_excel_csv2("E:/Users/pedro/Documents/ADM/ESTATISTICA/R/GLICEMIAS/glicemias_filtradas.csv", col_names = TRUE, na = " ")
+
 glicemias_filtd %>% 
-  write_excel_csv2("E:/Users/pedro/Downloads/glicemias_filtradas.csv", col_names = TRUE, na = " ")
+  write_excel_csv2(here::here("planilhas salvas", "glicemias_filtradas.csv", na = " ")) # mesmo caso do primeiro read_csv
 
 
 #glicemias_filtd %>% 
@@ -37,12 +41,16 @@ install.packages("writexl")
 library(writexl)
 
 
+#glicemias_filtd %>% 
+#  write_xlsx("E:/Users/pedro/Documents/ADM/ESTATISTICA/R/GLICEMIAS/glicemias_filtradas_x.xlsx")
+
 glicemias_filtd %>% 
-  write_xlsx("E:/Users/pedro/Downloads/glicemias_filtradas_x.xlsx")
+  write_xlsx(here::here("planilhas salvas", "glicemias_filtradas_x.xlsx")) # mesmissimo caso do wd pessoal
 
 
 
 #alternativa usando xlsx
 library(readxl)
-glicemias_x <- read_excel("E:/Users/pedro/Downloads/export(1).xls", col_types = c("date", "t", "skip", "d", "skip"))
+#glicemias_x <- read_excel("E:/Users/pedro/Documents/ADM/ESTATISTICA/R/GLICEMIAS/export(1).xls", col_types = c("date", "t", "skip", "d", "skip"))
+glicemias_x <- read_excel(here::here("export(1).xls", col_types = c("date", "t", "skip", "d", "skip")))
 View(glicemias_x)
